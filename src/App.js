7ec00a605241bffffb5bed688840563f982140e9
Prosper Osagie-Amayo <prosperosagieamayo@gmail.com>
2020-07-06 10:39:50 -0700

    map mock data

--- src/App.js
+++ src/App.js
@@ -8,36 +8,18 @@ function App() {
     <Container style={{ padding: "2rem" }}>
       <h1>Knowledge Check Block</h1>
       <Row style={{ padding: "1rem 0 1rem 0" }}>
-        <Col lg={4} style={{ margin: "1rem 0" }}>
-          <CheckBlock
-            text={<p>{mockData[0].text}</p>}
-            media={mockData[0].media}
-            mediaType={mockData[0].mediaType}
-            answers={mockData[0].answers}
-            correctAnswer={mockData[0].correctAnswer}
-            message={mockData[0].message}
-          />
-        </Col>
-
-        <Col lg={4} style={{ margin: "1rem 0" }}>
-          <CheckBlock
-            text={<p>{mockData[1].text}</p>}
-            media={mockData[1].media}
-            mediaType={mockData[1].mediaType}
-            answers={mockData[1].answers}
-            correctAnswer={mockData[1].correctAnswer}
-            message={mockData[1].message}
-          />
-        </Col>
-
-        <Col lg={4} style={{ margin: "1rem 0" }}>
-          <CheckBlock
-            text={<p>{mockData[2].text}</p>}
-            answers={mockData[2].answers}
-            correctAnswer={mockData[2].correctAnswer}
-            message={mockData[2].message}
-          />
-        </Col>
+        {mockData.map((data, i) => (
+          <Col lg={4} style={{ margin: "1rem 0" }}>
+            <CheckBlock
+              text={<p>{data.text}</p>}
+              media={data.media}
+              mediaType={data.mediaType}
+              answers={data.answers}
+              correctAnswer={data.correctAnswer}
+              message={data.message}
+            />
+          </Col>
+        ))}
       </Row>
     </Container>
   );
